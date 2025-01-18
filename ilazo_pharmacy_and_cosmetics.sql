a6c9888b3fb9ce57cfbe399746db0e990d112592
nyaniTz <cliffmlimwa@gmail.com>
2025-01-18 14:54:18 +0300

    good popup

--- ilazo_pharmacy_and_cosmetics.sql
+++ ilazo_pharmacy_and_cosmetics.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2022 at 11:32 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Dec 29, 2024 at 06:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,34 +24,188 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `receved items`
+-- Table structure for table `authentication`
 --
 
-CREATE TABLE `receved items` (
+CREATE TABLE `authentication` (
   `id` int(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `authentication`
+--
+
+INSERT INTO `authentication` (`id`, `Email`, `Password`) VALUES
+(1, 'nyoxmlimwa@gmail.com', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordergenerator`
+--
+
+CREATE TABLE `ordergenerator` (
+  `id` int(255) NOT NULL,
+  `OrderGenerators` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ordergenerator`
+--
+
+INSERT INTO `ordergenerator` (`id`, `OrderGenerators`) VALUES
+(1, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(255) NOT NULL,
+  `ProductId` varchar(255) NOT NULL,
   `Description` varchar(255) NOT NULL,
   `Unit` varchar(255) NOT NULL,
   `Quantity` int(255) NOT NULL,
   `Cost` int(255) NOT NULL,
-  `Amount` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `PurchasingQty` int(255) DEFAULT NULL,
+  `Amount` int(255) DEFAULT NULL,
+  `OrderID` varchar(255) DEFAULT NULL,
+  `Date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `receved items`
+-- Table structure for table `ordertracker`
 --
 
-INSERT INTO `receved items` (`id`, `Description`, `Unit`, `Quantity`, `Cost`, `Amount`) VALUES
-(1, 'panadol', 'mmm23', 23, 23, 23),
-(2, 'mimi', '233', 23, 33, 22);
+CREATE TABLE `ordertracker` (
+  `id` int(255) NOT NULL,
+  `ProductId` varchar(255) NOT NULL,
+  `OrderID` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `PurchasingQty` int(255) DEFAULT NULL,
+  `Amount` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recevedauthentication`
+--
+
+CREATE TABLE `recevedauthentication` (
+  `id` int(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `recevedauthentication`
+--
+
+INSERT INTO `recevedauthentication` (`id`, `Email`, `Password`) VALUES
+(1, '', 'kingkelly7898');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receveditems`
+--
+
+CREATE TABLE `receveditems` (
+  `id` int(255) NOT NULL,
+  `ProductId` varchar(255) NOT NULL,
+  `VendorName` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Unit` varchar(255) NOT NULL,
+  `Quantity` int(255) NOT NULL,
+  `PurchasingCost` int(255) NOT NULL,
+  `Cost` int(255) NOT NULL,
+  `Amount` int(255) DEFAULT NULL,
+  `RecevedDate` varchar(255) NOT NULL,
+  `ExpireDate` varchar(255) NOT NULL,
+  `Image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `receveditems`
+--
+
+INSERT INTO `receveditems` (`id`, `ProductId`, `VendorName`, `Description`, `Unit`, `Quantity`, `PurchasingCost`, `Cost`, `Amount`, `RecevedDate`, `ExpireDate`, `Image`) VALUES
+(1, 'PROD2', 'Nyau', 'Panadol', 'Ampoules', 54, 2, 12, 108, '18-11-2024 17:12:40 ', '2024-12-01', ''),
+(2, 'PROD2', 'Nyau', 'Aspirine', 'Capsules', 100, 2, 6, 200, '18-11-2024 17:13:08 ', '2025-05-08', ''),
+(11, 'PROD3', 'hello', 'panadol', 'Bottles', 233, 2, 1, 466, '26-11-2024 20:01:15', '2024-12-20', ''),
+(14, 'PROD12', '$ VendorName', 'Magnesium', 'Bottles', 123, 2, 3, 246, '27-11-2024 17:31:06', '2025-05-23', 'uploads/Designer.jpeg'),
+(15, 'PROD15', '$ VendorName', 'jee', 'Bottles', 2232, 12, 11, 26784, '27-11-2024 19:16:23', '2025-04-11', 'uploads/Designer.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userauthentication`
+--
+
+CREATE TABLE `userauthentication` (
+  `id` int(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userauthentication`
+--
+
+INSERT INTO `userauthentication` (`id`, `Email`, `Password`) VALUES
+(1, 'cliffmlimwa@gmail.com', '1');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `receved items`
+-- Indexes for table `authentication`
 --
-ALTER TABLE `receved items`
+ALTER TABLE `authentication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ordergenerator`
+--
+ALTER TABLE `ordergenerator`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ordertracker`
+--
+ALTER TABLE `ordertracker`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recevedauthentication`
+--
+ALTER TABLE `recevedauthentication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `receveditems`
+--
+ALTER TABLE `receveditems`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userauthentication`
+--
+ALTER TABLE `userauthentication`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -59,10 +213,46 @@ ALTER TABLE `receved items`
 --
 
 --
--- AUTO_INCREMENT for table `receved items`
+-- AUTO_INCREMENT for table `authentication`
 --
-ALTER TABLE `receved items`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `authentication`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ordergenerator`
+--
+ALTER TABLE `ordergenerator`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ordertracker`
+--
+ALTER TABLE `ordertracker`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recevedauthentication`
+--
+ALTER TABLE `recevedauthentication`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `receveditems`
+--
+ALTER TABLE `receveditems`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `userauthentication`
+--
+ALTER TABLE `userauthentication`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
